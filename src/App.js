c694032895a1096fe36a14e302cbcc18dbca5da2
rinlama <rinjinlama83@gmail.com>
2020-07-19 13:11:37 -0400

    init lifecycles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setState((d) => d + 1);
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(state);
+  }, [state]);
+
+  return <div className="App">{state}</div>;
 }
 
 export default App;
