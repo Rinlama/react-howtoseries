8d08135b6280b3fa85c08ae77ecc171f2928ea7c
rinlama <rinjinlama83@gmail.com>
2020-08-15 12:54:50 -0400

    How to bind state to combo box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [foodState, setFoodState] = useState("dumpling");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container p-5">
+      <select
+        className="custom-select"
+        value={foodState}
+        onChange={(e) => {
+          const selectedFood = e.target.value;
+          setFoodState(selectedFood);
+        }}
+      >
+        <option value="steak">Steak</option>
+        <option value="sandwich">Sandwich</option>
+        <option value="dumpling">Dumpling</option>
+      </select>
+      {foodState}
     </div>
   );
 }
